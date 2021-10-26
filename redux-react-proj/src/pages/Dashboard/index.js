75d170169ba27e77c030bd87b50ab07627be91fa
Parvathi Nair <nairparvathi18@gmail.com>
2021-10-26 17:47:27 +0530

    Create index.js

---
+++ redux-react-proj/src/pages/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+import './styles.scss';
+
+
+const Dashboard = props => {
+  return (
+      <h1>
+        you are logged in
+      </h1>
+  )
+};
+
+export default Dashboard;
